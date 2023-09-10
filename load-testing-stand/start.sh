57aeaeaa6fc89f97ac1fc4043094c360618b4d8d
e.zaripov <e.zaripov@tinkoff.ru>
2023-09-10 14:45:51 +0300

    Add yandex tank to grafana converter

---
+++ load-testing-stand/start.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+OPEN_GRAFANA=false
+
+err() {
+    echo "[$(date +'%Y-%m-%d %H:%M:%S%z')]: $@" >&2
+}
+
+log(){
+    echo "[$(date +'%Y-%m-%d %H:%M:%S%z')]: $@" >&1
+}
+
+#Open grafana in default browser
+openGrafana(){
+    timeout 60 bash -c '
+    until (curl --output /dev/null --silent --head --fail http://localhost:3000); do     
+    sleep 1; 
+    done;'
+    python -mwebbrowser http://localhost:3000/d/metrics/overload > /dev/null  &
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        "--open-grafana") shift
+                        OPEN_GRAFANA='true'
+                        ;;
+    esac
+    shift || true
+done
+
+log "Start grafana"
+docker-compose up -d
+
+#${OPEN_GRAFANA} && log "Open grafana" && openGrafana
+#
+#log "Start load testing"
+#docker-compose run --rm tank -qc load.yaml
--- simpleOneGetService/src/main/java/eduard/zaripov/simpleonegetservice/SimpleController.java
+++ simpleOneGetService/src/main/java/eduard/zaripov/simpleonegetservice/SimpleController.java
@@ -1,12 +1,17 @@
 package eduard.zaripov.simpleonegetservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+    private final Logger logger = LoggerFactory.getLogger(SimpleController.class);
+
     @GetMapping("/get-hello")
     public String getHello() {
+        logger.info("GET for hello");
         return "Hello!";
     }
 }
